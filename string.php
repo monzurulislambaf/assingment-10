985f47d102fdeef9b9c83c9f904fd7495d415cf7
monzurulislambaf <monzurulislambaf@gmail.com>
2024-08-23 07:12:41 +0600

    vowel count and reversed string

--- string.php
+++ string.php
@@ -1,2 +1,11 @@
 
+<?php
 $strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string){
+    $vowelCount = preg_match_all("/[aeiou]/", $string);
+    $reversedString = strrev($string);
+    echo "Original String: $string, ";
+    echo "Vowel Count: $vowelCount, ";
+    echo " Reversed String: $reversedString\n";
+}
